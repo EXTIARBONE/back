27459c01c6d91a7739fb0e82148e94bc71cdcf21
Noe MARLEIX <noe@marleix.com>
2023-03-03 09:12:12 +0100

    dev done, need to test with env variables

--- models/historicCarbonScore.ts
+++ models/historicCarbonScore.ts
@@ -2,16 +2,14 @@ import mongoose, {Document, Model, Schema} from "mongoose";
 
 
 export const scoreCarboneHistoricSchema = new Schema({
-    scpre: {
+    score: {
         type: Schema.Types.Number,
-        required: true
     },
     date: {
         type: Schema.Types.Date,
-        required: true,
     },
 }, {
-    collection: "coffees",
+    collection: "historicCarbonScore",
     timestamps: true,
     versionKey: false
 });
