72de074003dcf72e6a42c6d2531e9f7b4a6076cc
noeoxycode <76428537+noeoxycode@users.noreply.github.com>
2023-03-03 01:09:48 +0100

    patching addActionsAvailable

--- controllers/action.controller.ts
+++ controllers/action.controller.ts
@@ -80,7 +80,7 @@ export class ActionController {
         router.get('/', this.getAllActions.bind(this));
 
         //Post routes
-        router.post("/addActionsAvailable", this.addActionsAvailable.bind(this));
+        router.post("/addActionsAvailable", express.json(), this.addActionsAvailable.bind(this));
         router.post("/addAction", this.addAction.bind(this));
 
         router.delete("/deleteAction/:id", this.deleteAction.bind(this));
